5b8542342c7dcee5c0cf86b720fdb34aeb63b811
belek <belektolubaev@gmail.com>
2019-03-28 00:29:30 +0600

    Настройка index.js App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Key from './containers/Key/Key.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Key/>
+      </div>
+    );
+  }
+}
+
+export default App;
