ef21949db525305d8d5b109fadfdda2b87b05ff6
belek <belektolubaev@gmail.com>
2019-03-28 17:53:51 +0600

    Сделал фоновый цвет меняющийся изходя из ответа

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const Screen = props => {
-    return <div className="card mb-3 mt-3 mx-auto border-warning" style={{width: 200, height: 112}}>
+    return props.success === "Правильно!" ? <div className="card mb-3 mt-3 mx-auto border-warning bg-success" style={{width: 200, height: 112}}>
         <div className="card-body">
             <p className="card-text">{props.check}</p>
-            {props.success === "Правильно!" ? <p className="card-text text-dark bg-success rounded-pill">{props.success}</p> :
-                <p className="card-text text-dark bg-danger rounded-pill">{props.success}</p>}
+            <p className="card-text text-dark">{props.success}</p>
+        </div>
+    </div>: props.success === "Не правильно!" ? <div className="card mb-3 mt-3 mx-auto border-warning bg-danger" style={{width: 200, height: 112}}>
+        <div className="card-body">
+            <p className="card-text">{props.check}</p>
+            <p className="card-text text-dark">{props.success}</p>
+        </div>
+    </div>: <div className="card mb-3 mt-3 mx-auto border-warning" style={{width: 200, height: 112}}>
+        <div className="card-body">
+            <p className="card-text">{props.check}</p>
         </div>
     </div>
 };
